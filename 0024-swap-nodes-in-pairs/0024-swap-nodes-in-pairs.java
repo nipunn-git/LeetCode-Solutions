d768d15be7d68609922207681f2a997f51cd8539
Nipun <nipunndhiman@gmail.com>
2026-06-17 22:47:14 +0530

    Update 0024-swap-nodes-in-pairs.java

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        // Base case: if fewer than 2 nodes, return as is
+        
         if (head == null || head.next == null) {
             return head;
         }
