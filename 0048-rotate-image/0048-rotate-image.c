86a38d1c99228528f07ee31d33f528d34e7bfc12
Nipun <nipunndhiman@gmail.com>
2026-06-23 10:39:19 +0530

    Time: 0 ms (100%), Space: 11.1 MB (5.06%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.c
@@ -0,0 +1,23 @@
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    // transpose 
+    for(int i=0; i<matrixSize; i++)
+    {
+        for(int j=1+i ; j<matrixSize; j++)
+        {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix [j][i] = temp;
+        }
+    }
+
+    // reverse each row
+    for(int i=0; i<matrixSize; i++)
+    {
+        for(int j=0; j<matrixSize/2;j++)
+        {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][matrixSize-1-j];
+            matrix[i][matrixSize-1-j] = temp;
+        }
+    }
+}
