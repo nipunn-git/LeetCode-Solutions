24caa055e24d8fdb15bfa8158fb38e4b90cf372b
Nipun <nipunndhiman@gmail.com>
2026-06-28 12:08:22 +0530

    Time: 1 ms (60.76%), Space: 12.7 MB (19.41%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        result=[]
+
+        def backtrack(current, open, close):
+            if len(current) == 2*n:
+                result.append(current)
+                return
+
+            if open < n:
+                backtrack(current + "(" , open+1, close)
+
+            if close < open:
+                backtrack(current + ")" , open, close+1)
+
+        backtrack("", 0, 0)
+        return result
+        
