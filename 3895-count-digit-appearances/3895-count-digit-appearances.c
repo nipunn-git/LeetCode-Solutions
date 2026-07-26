6611458444f856fa0eb925f3618656c49e7f0415
Nipun <nipunndhiman@gmail.com>
2026-07-26 10:05:00 +0530

    Time: 2 ms (37.8%), Space: 10.5 MB (93.29%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.c
@@ -0,0 +1,13 @@
+int countDigitOccurrences(int* nums, int numsSize, int digit) {
+    int count=0;
+    for(int i=0; i<numsSize; i++)
+    {
+        while(nums[i] !=0)
+        {
+            int temp = nums[i]%10;
+            if(temp==digit) count++;
+            nums[i] /=10;
+        }
+    }
+    return count;
+}
