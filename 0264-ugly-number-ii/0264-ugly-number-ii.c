5bdd2edc8e9072f69c1dc6bf58493c02702cfd3e
Nipun <nipunndhiman@gmail.com>
2026-08-14 23:25:18 +0530

    Time: 2 ms (42.42%), Space: 10.4 MB (40.61%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.c
@@ -0,0 +1,30 @@
+int nthUglyNumber(int n) {
+    int *ugly = (int*)malloc(n * sizeof(int));
+    ugly[0] = 1;
+    
+    int i2 = 0, i3 = 0, i5 = 0;
+    int next2 = 2, next3 = 3, next5 = 5;
+    int nextUgly;
+    
+    for (int i = 1; i < n; i++) {
+        nextUgly = (next2 < next3) ? (next2 < next5 ? next2 : next5) : (next3 < next5 ? next3 : next5);
+        ugly[i] = nextUgly;
+        
+        if (nextUgly == next2) {
+            i2++;
+            next2 = ugly[i2] * 2;
+        }
+        if (nextUgly == next3) {
+            i3++;
+            next3 = ugly[i3] * 3;
+        }
+        if (nextUgly == next5) {
+            i5++;
+            next5 = ugly[i5] * 5;
+        }
+    }
+    
+    int result = ugly[n-1];
+    free(ugly);
+    return result;
+}
